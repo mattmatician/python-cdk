6eb710c0b317182fe93c47f5b92f8efe67a46a4a
Matthew Beaumont <matthewbeaumont@clarilis.com>
2022-03-31 21:45:13 +0200

    Add EC2 and DB in a VPC, this is taken from the AWS CDK example: https://github.com/aws-samples/aws-cdk-examples/tree/master/python/new-vpc-alb-asg-mysql

--- my-project/app.py
+++ my-project/app.py
@@ -1,28 +1,18 @@
 #!/usr/bin/env python3
-import os
 
-import aws_cdk as cdk
+from aws_cdk import App
 
-from my_project.my_project_stack import MyProjectStack
+from cdk_vpc_ec2.cdk_vpc_stack import CdkVpcStack
+from cdk_vpc_ec2.cdk_ec2_stack import CdkEc2Stack
+from cdk_vpc_ec2.cdk_rds_stack import CdkRdsStack
 
+app = App()
 
-app = cdk.App()
-MyProjectStack(app, "MyProjectStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+vpc_stack = CdkVpcStack(app, "cdk-vpc")
+ec2_stack = CdkEc2Stack(app, "cdk-ec2",
+                        vpc=vpc_stack.vpc)
+rds_stack = CdkRdsStack(app, "cdk-rds",
+                        vpc=vpc_stack.vpc,
+                        asg_security_groups=ec2_stack.asg.connections.security_groups)
 
 app.synth()
