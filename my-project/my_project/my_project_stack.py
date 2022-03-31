6401ee5c61838c610d72dcf87bc05bf0a3980657
Matthew Beaumont <matthewbeaumont@clarilis.com>
2022-03-31 20:14:17 +0200

    Add an EC2 instance inside a VPC. This is taken from the AWS CDK example: https://github.com/aws-samples/aws-cdk-examples/blob/master/python/ec2/instance/app.py

--- my-project/my_project/my_project_stack.py
+++ my-project/my_project/my_project_stack.py
@@ -1,6 +1,8 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_ec2 as ec2,
+    aws_iam as iam,
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -17,3 +19,30 @@ class MyProjectStack(Stack):
         #     self, "MyProjectQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+
+        # VPC
+        vpc = ec2.Vpc(self, "VPC",
+            nat_gateways=0,
+            subnet_configuration=[ec2.SubnetConfiguration(name="public",subnet_type=ec2.SubnetType.PUBLIC)]
+            )
+
+        # AMI
+        amzn_linux = ec2.MachineImage.latest_amazon_linux(
+            generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+            edition=ec2.AmazonLinuxEdition.STANDARD,
+            virtualization=ec2.AmazonLinuxVirt.HVM,
+            storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE
+            )
+
+        # Instance Role and SSM Managed Policy
+        role = iam.Role(self, "InstanceSSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
+
+        role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"))
+
+        # Instance
+        instance = ec2.Instance(self, "Instance",
+            instance_type=ec2.InstanceType("t3.nano"),
+            machine_image=amzn_linux,
+            vpc = vpc,
+            role = role
+            )
