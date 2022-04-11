30adb897e0cfb851d5ee1d555d59157f716af976
Matt Beaumont <github@beaum.xyz>
2022-04-11 16:59:26 +0100

    Comment out the means to deploy a second stack

--- my-project/app.py
+++ my-project/app.py
@@ -7,6 +7,6 @@ from my_project.my_project_stack import MyProjectStack
 app = App()
 
 my_project = MyProjectStack(app, "MyProjectStack")
-my_project2 = MyProjectStack(app, "MyProjectStack2")
+# my_project2 = MyProjectStack(app, "MyProjectStack2")
 
 app.synth()
